b58f284ab6f3e190e13f9c81f97d952939436a03
lcostenko <lcostenko.r4@gmail.com>
2023-03-23 11:59:49 +0200

    Update NoteDao.kt
    Add getNotesList() and insertNote()

--- app/src/main/java/com/example/notesapp/database/NoteDao.kt
+++ app/src/main/java/com/example/notesapp/database/NoteDao.kt
@@ -1,4 +1,19 @@
 package com.example.notesapp.database
 
-class NoteDao {
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.notesapp.pojo.NoteInfo
+
+@Dao
+interface NoteDao {
+
+    @Query("SELECT * FROM notes_table ORDER BY time DESC")
+    fun getNotesList(): LiveData<List<NoteInfo>>
+
+    // ?
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertNote(note: NoteInfo)
 }
