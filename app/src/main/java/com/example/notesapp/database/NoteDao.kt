798c968aad9a6686958dce84ca79f2edfb25f3e5
lcostenko <lcostenko.r4@gmail.com>
2023-03-23 14:08:51 +0200

    Update NoteDao.kt
    Add deleteNote()

--- app/src/main/java/com/example/notesapp/database/NoteDao.kt
+++ app/src/main/java/com/example/notesapp/database/NoteDao.kt
@@ -1,10 +1,7 @@
 package com.example.notesapp.database
 
 import androidx.lifecycle.LiveData
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
 import com.example.notesapp.pojo.NoteInfo
 
 @Dao
@@ -13,7 +10,9 @@ interface NoteDao {
     @Query("SELECT * FROM notes_table ORDER BY time DESC")
     fun getNotesList(): LiveData<List<NoteInfo>>
 
-    // ?
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertNote(note: NoteInfo)
+
+    @Delete
+    fun deleteNote(note: NoteInfo)
 }
