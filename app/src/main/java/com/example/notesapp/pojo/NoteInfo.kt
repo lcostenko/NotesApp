ecb4e610f9f5c1d3e2b8085e1002ee5c66a8fda2
lcostenko <lcostenko.r4@gmail.com>
2023-03-23 11:49:51 +0200

    Update NoteInfo.kt
    Create entity

--- app/src/main/java/com/example/notesapp/pojo/NoteInfo.kt
+++ app/src/main/java/com/example/notesapp/pojo/NoteInfo.kt
@@ -1,4 +1,13 @@
 package com.example.notesapp.pojo
 
-class NotesInfo {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "notes_table")
+data class NoteInfo(
+    @PrimaryKey
+    val id: Int?,
+    val title: String?,
+    val description: String?,
+    val time: Long?
+) { }
