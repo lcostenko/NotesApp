9efd70cd2878d1fcadee37a2d534f371bd109f41
lcostenko <lcostenko.r4@gmail.com>
2023-03-27 14:01:22 +0300

    Update NoteInfo.kt

--- app/src/main/java/com/example/notesapp/pojo/NoteInfo.kt
+++ app/src/main/java/com/example/notesapp/pojo/NoteInfo.kt
@@ -1,13 +1,22 @@
 package com.example.notesapp.pojo
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.example.notesapp.utils.convertMillisToTime
 
 @Entity(tableName = "notes_table")
 data class NoteInfo(
     @PrimaryKey
-    val id: Int?,
+    val id: Long?,
     val title: String?,
     val description: String?,
     val time: Long?
-) { }
+) {
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun getFormattedTime(): String {
+        return convertMillisToTime(time)
+    }
+}
