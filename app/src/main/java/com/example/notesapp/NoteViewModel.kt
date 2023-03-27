bd1fa0b55f0a34de64ce1e981954fa93867e3652
lcostenko <lcostenko.r4@gmail.com>
2023-03-27 14:00:45 +0300

    Update NoteViewModel.kt

--- app/src/main/java/com/example/notesapp/NoteViewModel.kt
+++ app/src/main/java/com/example/notesapp/NoteViewModel.kt
@@ -2,17 +2,24 @@ package com.example.notesapp
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.example.notesapp.database.AppDatabase
 import com.example.notesapp.pojo.NoteInfo
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class NoteViewModel(application: Application) : AndroidViewModel(application) {
 
     private val db = AppDatabase.getInstance(application)
+
+    private val _selectedItem = MutableLiveData<NoteInfo?>()
+    val selectedItem: LiveData<NoteInfo?> = _selectedItem
+
+    private var title: String? = selectedItem.value?.title
+    private var description: String? = selectedItem.value?.description
+    private var time: Long? = selectedItem.value?.time
+
     val notesList = db.noteDao().getNotesList()
 
     fun addNote(note: NoteInfo) {
@@ -25,5 +32,50 @@ class NoteViewModel(application: Application) : AndroidViewModel(application) {
         viewModelScope.launch {
             db.noteDao().deleteNote(note)
         }
+    }
+
+    fun changeTitle(title: String, cTIS: Long) {
+        this.title = title
+        time = cTIS
+    }
+
+    fun changeDescription(description: String, cTIS: Long) {
+        this.description = description
+        time = cTIS
+    }
+
+    fun buildNote(): NoteInfo {
+        val note = NoteInfo(
+            selectedItem.value?.id,
+            title,
+            description,
+            time
+        )
+        clearUpNote()
+        return if (hasChanges(selectedItem.value!!, note)) {
+            note
+        } else {
+            selectedItem.value!!
+        }
+    }
+
+    fun setUpNote() {
+        title = selectedItem.value?.title
+        description = selectedItem.value?.description
+        time = selectedItem.value?.time
+    }
+
+    private fun clearUpNote() {
+        title = null
+        description = null
+        time = null
+    }
+
+    private fun hasChanges(noteSelected: NoteInfo, noteBuilt: NoteInfo): Boolean {
+        return noteSelected.title != noteBuilt.title || noteSelected.description != noteBuilt.description
+    }
+
+    fun selectingNoteItem(note: NoteInfo?) {
+        _selectedItem.value = note
     }
 }
