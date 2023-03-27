a03d2a4e9af9bec335216e1578a25d2b1fd2b3b2
lcostenko <lcostenko.r4@gmail.com>
2023-03-27 14:03:28 +0300

    Update fragments.

--- app/src/main/java/com/example/notesapp/FirstFragment.kt
+++ app/src/main/java/com/example/notesapp/FirstFragment.kt
@@ -1,44 +1,35 @@
 package com.example.notesapp
 
-import android.content.Context
 import android.os.Bundle
-import android.provider.ContactsContract.CommonDataKinds.Note
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.notesapp.adapters.NoteAdapter
-import com.example.notesapp.database.AppDatabase
 import com.example.notesapp.databinding.FragmentFirstBinding
 import com.example.notesapp.pojo.NoteInfo
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import com.example.notesapp.utils.SwipeToDeleteCallback
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
+
 class FirstFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
-    private lateinit var viewModel: NoteViewModel
+    private val sharedViewModel by lazy {
+        ViewModelProvider(requireActivity())[NoteViewModel::class.java]
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -48,13 +39,18 @@ class FirstFragment : Fragment() {
         binding.rvNotesList.layoutManager = LinearLayoutManager(activity)
         binding.rvNotesList.adapter = adapter
 
-        viewModel = ViewModelProvider(this)[NoteViewModel::class.java]
-        viewModel.notesList.observe(viewLifecycleOwner, Observer {
+        val swipeToDeleteCallback = SwipeToDeleteCallback(adapter, sharedViewModel)
+        val itemTouchHelper = ItemTouchHelper(swipeToDeleteCallback)
+        itemTouchHelper.attachToRecyclerView(binding.rvNotesList)
+
+        sharedViewModel.notesList.observe(viewLifecycleOwner, Observer {
             adapter.noteList = it
         })
-
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        adapter.onNoteClickListener = object : NoteAdapter.OnNoteClickListener {
+            override fun onNoteClick(note: NoteInfo) {
+                sharedViewModel.selectingNoteItem(note).also { sharedViewModel.setUpNote() }
+                findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            }
         }
     }
 
--- app/src/main/java/com/example/notesapp/SecondFragment.kt
+++ app/src/main/java/com/example/notesapp/SecondFragment.kt
@@ -1,29 +1,26 @@
 package com.example.notesapp
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
+import androidx.lifecycle.ViewModelProvider
 import com.example.notesapp.databinding.FragmentSecondBinding
 
-/**
- * A simple [Fragment] subclass as the second destination in the navigation.
- */
 class SecondFragment : Fragment() {
 
     private var _binding: FragmentSecondBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+    private val sharedViewModel by lazy {
+        ViewModelProvider(requireActivity())[NoteViewModel::class.java]
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
+        setHasOptionsMenu(true)
         _binding = FragmentSecondBinding.inflate(inflater, container, false)
         return binding.root
 
@@ -32,8 +29,53 @@ class SecondFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        val noteItem = sharedViewModel.selectedItem.value
+        val editTextTitle = binding.otfTitle.editText
+        val editTextDescription = binding.otfDescription.editText
+        editTextTitle?.setText(noteItem?.title)
+        editTextDescription?.setText(noteItem?.description)
+
+        editTextTitle?.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val currentTimeInSeconds = System.currentTimeMillis()
+                s?.let {
+                    sharedViewModel.changeTitle(it.toString(), currentTimeInSeconds)
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        editTextDescription?.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val currentTimeInSeconds = System.currentTimeMillis()
+                s?.let {
+                    sharedViewModel.changeDescription(it.toString(), currentTimeInSeconds)
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_main, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        return when (item.itemId) {
+            R.id.action_save -> {
+                sharedViewModel.buildNote().also { sharedViewModel.addNote(it) }
+                requireActivity().onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 
